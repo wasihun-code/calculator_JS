4e04a14fa5759787b4f2d0c27cd10a8b04ec527f
wasihun-code <wasihunageru7947@gmail.com>
2022-11-06 11:54:26 +0530

    Add: Support for multi-digit numbers to be clicked

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let numberBtns = document.querySelectorAll('.number');
 let operatorBtns = document.querySelectorAll('.operator')
 let displayed = document.querySelector('.display');
+let number;
+let counter = 0;
+let operator;
 // console.log(displayed);
 
-numberBtns.forEach(number => {
-    number.addEventListener('click', function(e) {
+numberBtns.forEach(numberBtn => {
+    numberBtn.addEventListener('click', function(e) {
         lognumber(e)
     })
 });
 
-operatorBtns.forEach(operator => {
-    operator.addEventListener('click', function(e) {
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', function(e) {
         logOperator(e);
     })
 })
 function lognumber(e) {
-    let currentNumber = e.target.innerText;
-    displayed.innerText = currentNumber;
-    console.log(displayed)
+    let currentNumber = parseInt(e.target.innerText);
+
+    if (counter === 0){
+        number = currentNumber;
+        counter++;
+    }
+    else {
+        number = number * 10 + currentNumber;
+    }
+    displayed.innerText = number;
 }
 
 function logOperator(e) {
